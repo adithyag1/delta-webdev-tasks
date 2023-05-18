f0a553599422a5882664065cb6d2b8cca8f98ae6
Adithya G <121248641+adithyag1@users.noreply.github.com>
2023-05-18 12:17:48 +0530

    Update snake.js

--- task1/normal/snake.js
+++ task1/normal/snake.js
@@ -230,6 +230,5 @@ view_button.onclick = function() {
     else{
         leaderboard_div.style.display="none";
         wrapper.style.display="flex";
-        sizeform.style.display="block";
     }
 }
