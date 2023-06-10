bad4160ea6988affa5bfee0828a31cddbd3ea5c7
Adithya G <121248641+adithyag1@users.noreply.github.com>
2023-06-10 10:42:14 +0530

    Update droid.js

--- task2/normal/droid.js
+++ task2/normal/droid.js
@@ -1,6 +1,6 @@
 const canvas=document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const size = Math.min(window.innerWidth, window.innerHeight)*0.95;
+const ctx=canvas.getContext("2d");
+const size=Math.min(window.innerWidth,window.innerHeight)*0.95;
 
 //take relative to the window size
 canvas.width=size;
@@ -53,10 +53,10 @@ function shoot(event){
     let dely=event.clientY-rect.top-player.y;
     let pyth=Math.sqrt(delx*delx+dely*dely);
     const bullet={
-        x: player.x,
-        y: player.y,
-        dx: delx/pyth*8,
-        dy: dely/pyth*8,
+        x:player.x,
+        y:player.y,
+        dx:delx/pyth*8,
+        dy:dely/pyth*8,
         kills:0
     };
     bullets.push(bullet);
@@ -76,67 +76,67 @@ function drawline(event){
 }
 
 function draw_everything(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const gradient = ctx.createRadialGradient(size/2, size/2, 0, size/2, size/2, size/2);
-    gradient.addColorStop(0, "rgb(112,58,110)"); // Start color at the center
-    gradient.addColorStop(1, "rgb(85,51,90)"); // End color at the outer circle
-    ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, size, size);
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    const gradient=ctx.createRadialGradient(size/2,size/2,0,size/2,size/2,size/2);
+    gradient.addColorStop(0,"rgb(112,58,110)"); // Start color at the center
+    gradient.addColorStop(1,"rgb(85,51,90)"); // End color at the outer circle
+    ctx.fillStyle=gradient;
+    ctx.fillRect(0,0,size,size);
 
-    const player_rem = new Path2D();
-    player_rem.rect(size * 0.01, size * 0.01, size * 0.4, size * 0.025);
-    ctx.fillStyle = "rgb(229,70,69)";
+    const player_rem=new Path2D();
+    player_rem.rect(size*0.01,size*0.01,size*0.4,size*0.025);
+    ctx.fillStyle="rgb(229,70,69)";
     ctx.fill(player_rem);
     player_rem.closePath();
 
 
-    const player_bar = new Path2D();
-    player_bar.rect(size * 0.01 + player.health * size * 0.4 / 10, size * 0.01, size * 0.4 - player.health*size*0.4/10, size * 0.025);
-    ctx.fillStyle = "rgb(200,200,200)";
+    const player_bar=new Path2D();
+    player_bar.rect(size*0.01+player.health*size*0.4 / 10,size*0.01,size*0.4-player.health*size*0.4/10,size*0.025);
+    ctx.fillStyle="rgb(200,200,200)";
     ctx.fill(player_bar);
     player_bar.closePath();
 
-    ctx.fillStyle = "white";
-    ctx.font = `${size * 0.02}px Arial`;
-    ctx.fillText(`SCORE: ${score}`, size * 0.45, size * 0.03);
+    ctx.fillStyle="white";
+    ctx.font=`${size*0.02}px Arial`;
+    ctx.fillText(`SCORE:${score}`,size*0.45,size*0.03);
 
-    ctx.fillStyle = "white";
-    ctx.font = `${size * 0.02}px Arial`;
-    ctx.fillText("PLAYER HEALTH", size * 0.02, size * 0.03);
+    ctx.fillStyle="white";
+    ctx.font=`${size*0.02}px Arial`;
+    ctx.fillText("PLAYER HEALTH",size*0.02,size*0.03);
 
-    const home_rem = new Path2D();
-    home_rem.rect(size * 0.59, size * 0.01, size * 0.4, size * 0.025);
-    ctx.fillStyle = "rgb(229,70,69)";
+    const home_rem=new Path2D();
+    home_rem.rect(size*0.59,size*0.01,size*0.4,size*0.025);
+    ctx.fillStyle="rgb(229,70,69)";
     ctx.fill(home_rem);
     home_rem.closePath();
 
-    const home_bar = new Path2D();
-    home_bar.rect(size * 0.59 + home_health * size * 0.4 / 10, size * 0.01, size * 0.4 - home_health*size*0.4/10, size * 0.025);
-    ctx.fillStyle = "rgb(200,200,200)";
+    const home_bar=new Path2D();
+    home_bar.rect(size*0.59+home_health*size*0.4 / 10,size*0.01,size*0.4-home_health*size*0.4/10,size*0.025);
+    ctx.fillStyle="rgb(200,200,200)";
     ctx.fill(home_bar);
     home_bar.closePath();
 
-    ctx.fillStyle = "white";
-    ctx.font = `${size * 0.02}px Arial`;
-    ctx.fillText("HOME HEALTH", size * 0.6, size * 0.03);
+    ctx.fillStyle="white";
+    ctx.font=`${size*0.02}px Arial`;
+    ctx.fillText("HOME HEALTH",size*0.6,size*0.03);
 
-    ctx.rect(size * 0.3, size * 0.7, size * 0.4, size * 0.1);
-    ctx.fillStyle = "yellow";
+    ctx.rect(size*0.3,size*0.7,size*0.4,size*0.1);
+    ctx.fillStyle="yellow";
     ctx.fill();
 
-    ctx.fillStyle = "black";
-    ctx.font = `bold ${size * 0.03}px Arial`;
-    ctx.fillText("HOME", size * 0.5 - size * 0.1 + size * 0.06, size * 0.7 + size * 0.06);
+    ctx.fillStyle="black";
+    ctx.font=`bold ${size*0.03}px Arial`;
+    ctx.fillText("HOME",size*0.5-size*0.1+size*0.06,size*0.7+size*0.06);
 
-    ctx.fillStyle = "rgb(229,70,69)";
+    ctx.fillStyle="rgb(229,70,69)";
     for(let i=0;i<bots.length;i++){
-        ctx.fillRect(bots[i][0], bots[i][1], size * 0.03, size * 0.03);
-        bots[i][1] += size * 0.003;
+        ctx.fillRect(bots[i][0],bots[i][1],size*0.03,size*0.03);
+        bots[i][1] += size*0.003;
     }
 
-    ctx.fillStyle = "rgb(208,78,208)";
+    ctx.fillStyle="rgb(208,78,208)";
     for(let i=0;i<bullets.length;i++){
-        ctx.fillRect(bullets[i].x, bullets[i].y, size * 0.02, size * 0.02);
+        ctx.fillRect(bullets[i].x,bullets[i].y,size*0.02,size*0.02);
         bullets[i].x+=bullets[i].dx;
         bullets[i].y+=bullets[i].dy;
         if(!(bullets[i].y>0&&bullets[i].y<size&&bullets[i].x>0&&bullets[i].x<size)){
@@ -148,20 +148,20 @@ function draw_everything(){
         }
     }
 
-    if (player.x < size * 1.05 && player.x > -size * 0.05) {
-        let player_body = new Path2D();
-        player_body.arc(player.x, player.y, size * 0.05, 0, Math.PI * 2, true);
-        ctx.fillStyle = "rgb(183,19,115)";
+    if (player.x < size*1.05 && player.x > -size*0.05) {
+        let player_body=new Path2D();
+        player_body.arc(player.x,player.y,size*0.05,0,Math.PI*2,true);
+        ctx.fillStyle="rgb(183,19,115)";
         ctx.fill(player_body);
         player_body.closePath();
     }
 
     else {
-        if (player.x >= size * 1.05) {
-            player.x = -size * 0.05;
+        if (player.x >= size*1.05) {
+            player.x=-size*0.05;
         }
-        else if (player.x <= -size * 0.05) {
-            player.x = size * 1.05;
+        else if (player.x <= -size*0.05) {
+            player.x=size*1.05;
         }
     }
 
@@ -170,12 +170,12 @@ function draw_everything(){
             //check if bullet hit the bot
             try{
                 if (
-                    bullets[j].x < bots[i][0] + size * 0.03 &&//bot height, width=size*0.03
-                    bullets[j].x + size * 0.02 > bots[i][0] &&//bullet height, width=size*0.02
-                    bullets[j].y < bots[i][1] + size * 0.03&&
-                    bullets[j].y + size * 0.02 > bots[i][1]
+                    bullets[j].x < bots[i][0]+size*0.03 &&//bot height,width=size*0.03
+                    bullets[j].x+size*0.02 > bots[i][0] &&//bullet height,width=size*0.02
+                    bullets[j].y < bots[i][1]+size*0.03&&
+                    bullets[j].y+size*0.02 > bots[i][1]
                 ) {
-                    bots.splice(i, 1);
+                    bots.splice(i,1);
                     bullets[j].kills++;
                     score+=4;
                 }
@@ -186,9 +186,9 @@ function draw_everything(){
 
             //check if it hits the home base and decrease home health
             if (
-                size*0.7 < bots[i][1] + size * 0.03 &&
+                size*0.7 < bots[i][1]+size*0.03 &&
                 size*0.8 > bots[i][1] &&
-                size*0.3 < bots[i][0] + size * 0.03 &&
+                size*0.3 < bots[i][0]+size*0.03 &&
                 size*0.7 > bots[i][0]
                 )
             {
@@ -198,9 +198,9 @@ function draw_everything(){
 
             //check if it hits the player and decrease playerhealth
             if (
-                player.y-size*0.05 < bots[i][1] + size * 0.03 &&
+                player.y-size*0.05 < bots[i][1]+size*0.03 &&
                 player.y+size*0.05 > bots[i][1] &&
-                player.x-size*0.05 < bots[i][0] + size * 0.03 &&
+                player.x-size*0.05 < bots[i][0]+size*0.03 &&
                 player.x+size*0.05 > bots[i][0]
                 )
             {
@@ -227,14 +227,14 @@ game_interval=setInterval(draw_everything,15);
 
 function reloadPage() {
     clearInterval(game_interval);
-    alert("Game over! Your score: " + score);
+    alert("Game over! Your score:"+score);
     location.reload(true);
 }
 
 document.addEventListener("keydown",move_player);
 document.addEventListener("mousedown",shoot);
 
-const rect = canvas.getBoundingClientRect();
+const rect=canvas.getBoundingClientRect();
 //rect gets the position of canvas top left point
 
 document.addEventListener("mousemove",drawline);
